42709fc6c9254f536e46fb9e3425c96c98597661
Raki <killer.vp.mic@gmail.com>
2023-10-14 10:24:13 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [
+    "favicon.svg",
+    "favicon.ico",
+    "robots.txt",
+    "apple-touch-icon.png",
+  ],
+  manifest: {
+    name: "Fast QR",
+    short_name: "Fast QR",
+    description: "Fast QR maker to save and generate QRs as PNGs",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/book-landing-page/",
+  plugins: [react(), VitePWA(manifestForPlugin)],
+  base: "./",
 });
